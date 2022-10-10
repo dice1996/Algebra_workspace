723ebf3849658f4eb2b9edda4d78caa88a10dd5c
Dino Cerjak <d.cerjak@yahoo.com>
2022-10-10 21:38:03 +0200

    new file:   Algebra/MojeVjezbe/zadatak2.py 	new file:   Algebra/MojeVjezbe/zadatak3.py

---
+++ Algebra/MojeVjezbe/zadatak2.py
@@ -0,0 +1,6 @@
+vowels = ["a", "e", "i", "o", "u"]
+
+if input("Unesi slovo: ") in vowels:
+    print("Uneseno slovo je samoglasnik!")
+else:
+    print("Uneseno slovo je suglasnik!")
---
+++ Algebra/MojeVjezbe/zadatak3.py
@@ -0,0 +1,22 @@
+"""Write a program to count the occurence of even number and odd number between the range"""
+
+prviBroj = int(input("Unesi prvi broj raspona: "))
+drugiBroj = int(input("Unesi drugi broj raspona: "))
+
+if drugiBroj < prviBroj:
+    print("\nDrugi broj ne smije biti manji od prvog broja! Radim zamjenu varijabli...\n")
+    pomocna = prviBroj
+    prviBroj = drugiBroj
+    drugiBroj = pomocna
+    print("Zamjena napravljena. Nastavljam program....\n")
+
+even = 0
+odd = 0
+
+for broj in range(prviBroj, drugiBroj+1):
+    if broj%2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print(f"\nBroj parnih brojeva je {even}\nBroj neparnih brojeva je {odd}\nRaspon brojeva je ({prviBroj}, {drugiBroj})")
