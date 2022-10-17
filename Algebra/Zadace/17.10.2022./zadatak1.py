9391367a2e234c987bf517bc44d55309cd8dc3fa
Dino Cerjak <d.cerjak@yahoo.com>
2022-10-17 23:49:07 +0200

    new file:   Algebra/Vjezbe/11.10.2022./17.10.2022./dictionary.ipynb 	new file:   Algebra/Zadace/17.10.2022./zadatak1.py

---
+++ Algebra/Zadace/17.10.2022./zadatak1.py
@@ -0,0 +1,8 @@
+"""
+Zadatak 11:
+Napisati program gdje unosite 5 predmeta (predmet ima propertye: ime i ocjena). Nakon unosa ispisati ime predmeta s najnizom ocjenom i prosjek ocjena svih predmeta 
+"""
+listaOcjena = ["1", "2", "3", "4", "5"]
+predmeti = {}
+BROJ_PREDMETA = 5
+
