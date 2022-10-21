7e225b4588ca7dae69b29447c432be57f99d4e2b
Dino Cerjak <d.cerjak@yahoo.com>
2022-10-21 18:32:12 +0200

    new file:   Algebra/Vjezbe/funkcije/pomocne_funkcije.py 	new file:   Algebra/Vjezbe/funkcije/zadatak4.py

---
+++ Algebra/Vjezbe/funkcije/pomocne_funkcije.py
@@ -0,0 +1,32 @@
+def unosBroja(listaBrojeva, brojPonavljanja):
+    for i in range(brojPonavljanja):
+        flag = 1
+        while flag:
+            try:
+                unos = int(input(f"Unesi {i + 1}. cijeli broj: "))
+                flag = 0
+                listaBrojeva.append(unos)
+            except:
+                print("Uneseno nije broj! Pokušaj ponovno!")
+    return listaBrojeva
+def brojPonavljanja():
+    flag = 1
+    while flag:
+        try:
+            brojUnosa = int(input("Unesi koliko brojeva zelis upisati: "))
+            if brojUnosa >= 0:
+                flag = 0
+            else:
+                print("Broj ponavljanja ne moze biti negativan! Pokusaj ponovno!")
+        except:
+            print("Uneseno nije broj! Pokušaj ponovno!")
+    return brojUnosa
+def provjera(listaBrojeva: list):
+    par = 0
+    nepar = 0
+    for item in listaBrojeva:
+        if item%2 == 0:
+            par += 1
+        else:
+            nepar += 1
+    return par, nepar
---
+++ Algebra/Vjezbe/funkcije/zadatak4.py
@@ -0,0 +1,14 @@
+from pomocne_funkcije import unosBroja, brojPonavljanja, provjera
+counterPar = 0
+counterNepar = 0
+
+listaBrojeva = []
+
+brojUnosa = brojPonavljanja()
+listaBrojeva = unosBroja(listaBrojeva, brojUnosa)
+counterPar, counterNepar = provjera(listaBrojeva)
+    
+if listaBrojeva == []:
+    print("Nista nije uneseno! Program zavrsava...")
+else:
+    print(f"Broj parnih brojeva je {counterPar}, a neparnih {counterNepar}.")
