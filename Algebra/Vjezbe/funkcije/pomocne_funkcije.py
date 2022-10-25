f1b1e22c3be21cfeee1dd8abb4a302a0f340d604
Dino Cerjak <d.cerjak@yahoo.com>
2022-10-25 18:15:16 +0200

    modified:   Algebra/Vjezbe/funkcije/__pycache__/pomocne_funkcije.cpython-310.pyc 	modified:   Algebra/Vjezbe/funkcije/pomocne_funkcije.py 	modified:   Algebra/Vjezbe/funkcije/zadatak11.py

--- Algebra/Vjezbe/funkcije/pomocne_funkcije.py
+++ Algebra/Vjezbe/funkcije/pomocne_funkcije.py
@@ -42,31 +42,3 @@ def countParNepar(listaBrojeva: list):
         else:
             nepar += 1
     return par, nepar
-
-def unosPredmeta(predmeti : dict, BROJ_PREDMETA, listaOcjena):
-    for i in range(BROJ_PREDMETA):
-        while True:
-            ime = input("Unesi ime predmeta: ").upper()
-            if ime not in predmeti.keys():
-                ocjena = unesiCijeliBroj(f"Unesi ocjenu za predmet {ime}: ")
-                if ocjena in listaOcjena:
-                    predmeti[ime] = int(ocjena)  
-                    break              
-                else:
-                    print("Nije unesena ispravna ocjena. Pokusaj ponovno!")
-            else:
-                print("Ponovi unos jer je ovaj predmet već unesen!")
-    return predmeti
-
-def provjeraPredmeta (predmeti: dict, suma):
-    najnizaOcjena = []
-    for predmet in predmeti:
-        if najnizaOcjena == []:
-            najnizaOcjena.append(predmet) 
-            najnizaOcjena.append(predmeti[predmet])
-        elif najnizaOcjena[1] > predmeti[predmet]:
-            najnizaOcjena.clear()
-            najnizaOcjena.append(predmet) 
-            najnizaOcjena.append(predmeti[predmet])
-    suma += predmeti[predmet]
-    return najnizaOcjena, suma
--- Algebra/Vjezbe/funkcije/zadatak11.py
+++ Algebra/Vjezbe/funkcije/zadatak11.py
@@ -5,11 +5,42 @@ najnizaOcjena = []
 prosjekOcjena = None
 predmeti = {}
 suma = 0
+
+def unosPredmeta(predmeti : dict, BROJ_PREDMETA, listaOcjena):
+    for i in range(BROJ_PREDMETA):
+        while True:
+            ime = input("Unesi ime predmeta: ").upper()
+            if ime not in predmeti.keys():
+                ocjena = unesiCijeliBroj(f"Unesi ocjenu za predmet {ime}: ")
+                if ocjena in listaOcjena:
+                    predmeti[ime] = int(ocjena)  
+                    break              
+                else:
+                    print("Nije unesena ispravna ocjena. Pokusaj ponovno!")
+            else:
+                print("Ponovi unos jer je ovaj predmet već unesen!")
+    return predmeti
+
+def provjeraPredmeta (predmeti: dict, suma):
+    najnizaOcjena = []
+    for predmet in predmeti:
+        if najnizaOcjena == []:
+            najnizaOcjena.append(predmet) 
+            najnizaOcjena.append(predmeti[predmet])
+            suma = predmeti[predmet]
+        elif najnizaOcjena[1] > predmeti[predmet]:
+            najnizaOcjena.clear()
+            najnizaOcjena.append(predmet) 
+            najnizaOcjena.append(predmeti[predmet])
+            suma += predmeti[predmet]
+    
+    return najnizaOcjena, suma
+
 BROJ_PREDMETA = brojPonavljanja("Unesi broj predmeta: ")
 predmeti = unosPredmeta(predmeti, BROJ_PREDMETA, listaOcjena)
 najnizaOcjena, suma = provjeraPredmeta(predmeti, suma)
 
-prosjekOcjena = suma / predmeti.__len__()
+prosjekOcjena = suma / BROJ_PREDMETA
 
 print(f"Predmet s najnižom ocjenom {najnizaOcjena[1]} je {najnizaOcjena[0]}.")
 print(f"Prosjek svih unesenih ocjena je {prosjekOcjena}")
