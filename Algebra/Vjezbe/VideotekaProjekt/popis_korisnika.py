203426bca22e4a2a48b5baece143d0f31b2b3900
Dino Cerjak <d.cerjak@yahoo.com>
2022-11-01 13:13:33 +0100

    new file:   Algebra/Vjezbe/VideotekaProjekt/popis_korisnika.py 	new file:   Algebra/Vjezbe/VideotekaProjekt/videoteka.py

---
+++ Algebra/Vjezbe/VideotekaProjekt/popis_korisnika.py
@@ -0,0 +1,151 @@
+popis_korisnika = [{
+  "id": 1,
+  "ime": "Bunny Driutti",
+  "vrijeme_kreiranja": "2022-10-03",
+  "stanje_racuna": 96.09
+}, {
+  "id": 2,
+  "ime": "Cheston Hawe",
+  "vrijeme_kreiranja": "2022-07-10",
+  "stanje_racuna": 56.79
+}, {
+  "id": 3,
+  "ime": "Keary Gawn",
+  "vrijeme_kreiranja": "2022-03-07",
+  "stanje_racuna": 225.89
+}, {
+  "id": 4,
+  "ime": "Kylie Dilrew",
+  "vrijeme_kreiranja": "2022-08-04",
+  "stanje_racuna": 153.45
+}, {
+  "id": 5,
+  "ime": "Jaquenette Mistry",
+  "vrijeme_kreiranja": "2021-12-03",
+  "stanje_racuna": 115.73
+}, {
+  "id": 6,
+  "ime": "Dalis Muschette",
+  "vrijeme_kreiranja": "2022-03-16",
+  "stanje_racuna": 234.71
+}, {
+  "id": 7,
+  "ime": "Lowe Garie",
+  "vrijeme_kreiranja": "2021-12-25",
+  "stanje_racuna": 112.25
+}, {
+  "id": 8,
+  "ime": "Branden Hackney",
+  "vrijeme_kreiranja": "2021-12-06",
+  "stanje_racuna": 184.51
+}, {
+  "id": 9,
+  "ime": "Isobel Duchesne",
+  "vrijeme_kreiranja": "2022-07-23",
+  "stanje_racuna": 112.47
+}, {
+  "id": 10,
+  "ime": "Marguerite Charlton",
+  "vrijeme_kreiranja": "2022-01-16",
+  "stanje_racuna": 133.15
+}, {
+  "id": 11,
+  "ime": "Curr Ivanishin",
+  "vrijeme_kreiranja": "2022-10-30",
+  "stanje_racuna": 198.5
+}, {
+  "id": 12,
+  "ime": "Roda Rattray",
+  "vrijeme_kreiranja": "2021-12-11",
+  "stanje_racuna": 125.49
+}, {
+  "id": 13,
+  "ime": "Drucy Wallas",
+  "vrijeme_kreiranja": "2022-03-30",
+  "stanje_racuna": 226.11
+}, {
+  "id": 14,
+  "ime": "Silvana McSaul",
+  "vrijeme_kreiranja": "2022-06-29",
+  "stanje_racuna": 259.38
+}, {
+  "id": 15,
+  "ime": "Borg Hawksby",
+  "vrijeme_kreiranja": "2022-01-23",
+  "stanje_racuna": 69.55
+}, {
+  "id": 16,
+  "ime": "Willetta Ashingden",
+  "vrijeme_kreiranja": "2022-04-30",
+  "stanje_racuna": 88.36
+}, {
+  "id": 17,
+  "ime": "Ardyce Ashpital",
+  "vrijeme_kreiranja": "2022-05-21",
+  "stanje_racuna": 211.67
+}, {
+  "id": 18,
+  "ime": "Zulema Sunners",
+  "vrijeme_kreiranja": "2021-11-23",
+  "stanje_racuna": 108.11
+}, {
+  "id": 19,
+  "ime": "Feodora Darmody",
+  "vrijeme_kreiranja": "2022-03-28",
+  "stanje_racuna": 197.83
+}, {
+  "id": 20,
+  "ime": "Zolly Grimmert",
+  "vrijeme_kreiranja": "2022-07-18",
+  "stanje_racuna": 65.7
+}, {
+  "id": 21,
+  "ime": "Jehanna Rawles",
+  "vrijeme_kreiranja": "2022-06-19",
+  "stanje_racuna": 101.9
+}, {
+  "id": 22,
+  "ime": "Jan Van Der Vlies",
+  "vrijeme_kreiranja": "2022-05-30",
+  "stanje_racuna": 145.4
+}, {
+  "id": 23,
+  "ime": "Heindrick Corradino",
+  "vrijeme_kreiranja": "2022-06-11",
+  "stanje_racuna": 124.67
+}, {
+  "id": 24,
+  "ime": "Teddy Figger",
+  "vrijeme_kreiranja": "2022-07-26",
+  "stanje_racuna": 133.83
+}, {
+  "id": 25,
+  "ime": "Gasparo Elderidge",
+  "vrijeme_kreiranja": "2021-11-07",
+  "stanje_racuna": 76.17
+}, {
+  "id": 26,
+  "ime": "Myrvyn Francie",
+  "vrijeme_kreiranja": "2022-01-24",
+  "stanje_racuna": 150.86
+}, {
+  "id": 27,
+  "ime": "Lorenzo Krystof",
+  "vrijeme_kreiranja": "2022-09-19",
+  "stanje_racuna": 57.25
+}, {
+  "id": 28,
+  "ime": "Boyce Quenby",
+  "vrijeme_kreiranja": "2022-09-20",
+  "stanje_racuna": 246.03
+}, {
+  "id": 29,
+  "ime": "Fara Papis",
+  "vrijeme_kreiranja": "2022-07-26",
+  "stanje_racuna": 234.59
+}, {
+  "id": 30,
+  "ime": "Hersh Goalby",
+  "vrijeme_kreiranja": "2022-09-08",
+  "stanje_racuna": 222.39
+}]
---
+++ Algebra/Vjezbe/VideotekaProjekt/videoteka.py
@@ -0,0 +1,162 @@
+"""
+Kreirati softver za videoteku. Arhitektura je proizvoljna.
+
+id_naloga
+
+kreirati u formatu npr: 001-ID_KORISNIKA-ID_FILMA-GODINA
+
+001-025-018-2022
+
+"""
+import time
+from datetime import datetime as dt
+from prettytable import PrettyTable, ALL
+import os
+from popis_filmova import *
+from popis_korisnika import *
+
+izdani_filmovi = [
+
+    ]
+
+glavniIzbornik = {
+    1: "Ispis svih članova",
+    2: "Ispis posuđenih filmova",
+    3: "Posudi film",
+    4: "Vrati film",
+    0: "Izlaz iz aplikacije"
+}
+
+def ocisti_ekran():
+    os.system("cls" if os.name == "nt" else "clear")
+
+def unesiCijeliBroj(text = None, index = None):
+    while True:
+        try:
+            if text is None and index is None:
+                return int(input("Unesi broj: "))
+            elif text is None and index is not None:
+                return int(input(f"Unesi {index}. broj: "))
+            else:
+                return int(input(text))
+        except:
+            print("Krivi unos")
+
+def ispisIzbornika(izbornik):
+    for item in izbornik:
+        print(f"{item}. {izbornik[item]}")
+
+def unosOpcije(izbornik):
+    while True:
+        print("GLAVNI IZBORNIK\n")
+        ispisIzbornika(izbornik)
+        izbor = unesiCijeliBroj("\nUnesi opciju iz gornjeg izbornika: ")
+        if izbor not in izbornik.keys():
+            print("Unije validan unos! Pokusaj ponovno!")
+            time.sleep(2)
+            ocisti_ekran()
+        else:
+            if izbor == 0:
+                ocisti_ekran()
+                print("Kraj programa!")
+                return 0
+            else:
+                ocisti_ekran()
+                return izbor
+
+def funkcijaIspisa(lista: list, index = None):
+    ocisti_ekran()
+    if index == None:
+        t = PrettyTable(["ID", "IME", "SRANJE RAČUNA", "VRIJEME UČLANJENJA"], padding_width = 3, hrules=ALL)
+        print(f"Popis sadrži {len(lista)} članova:")
+        for item in lista:
+            t.add_row([str(item["id"]).zfill(3), item["ime"], item["stanje_racuna"], dt.fromisoformat(item["vrijeme_kreiranja"]).strftime("%d.%m.%Y.")])
+    else:
+        t = PrettyTable(["ID NALOGA", "IME KUPCA", "IME FILMA", "VRIJEME POSUDBE", "FILM VRAĆEN"], padding_width = 3, hrules=ALL)
+        for mainItem in lista:
+            t.add_row([str(mainItem["id_naloga"]), mainItem["ime_kupca"], mainItem["ime_filma"], dt.fromisoformat(mainItem["vrijeme_kreiranja"]).strftime("%d.%m.%Y."), ""])
+    print(f"{t}\n")
+
+def posudiFilm(lista: list):
+    ocisti_ekran()
+    novi_nalog = {}
+    izbor = unesiCijeliBroj("Unesi ID člana koji pouđuje film: ")
+    flag = False
+    for item in popis_korisnika:
+        if item["id"] == izbor:
+            flag = True
+   
+    if flag == False:
+        print("Pogresan ID. Pokusaj ponovno...")
+    else:
+        flag = False 
+        odluka = search(input("Unesi jednu do dvije riječi imena filma: ")) 
+        if odluka == 0:
+            print("Nema filma na popisu. Povratak u glavni izbornik...")
+            time.sleep(2)
+            ocisti_ekran()
+        else:
+            izbor_film = unesiCijeliBroj("Unesi ID filma koji član posuđuje (unesi 0 za prekid): ")
+            for item in popis_filmova:
+                if izbor_film == item["id"]:
+                    flag = True
+            if flag == False:
+                if izbor_film == 0:
+                    print("Povratak u glavni izbornik...")
+                else:
+                    print("Pogresan ID. Pokusaj ponovno...")
+                time.sleep(2)
+                ocisti_ekran()
+            else:
+                if lista == []:
+                    novi_nalog["id_naloga"] = str(1).zfill(3) + "-" + str(izbor).zfill(3) + "-" + str(izbor_film).zfill(3) + "-" + dt.now().strftime("%Y")
+                else:
+                    pomocna = lista[-1]["id_naloga"]
+                    pomocna_lista = pomocna.split("-")
+                    novi_nalog["id_naloga"] = str(int(pomocna_lista[0]) + 1).zfill(3) + "-" + str(izbor).zfill(3) + "-" + str(izbor_film).zfill(3) + "-" + dt.now().strftime("%Y")
+                
+                novi_nalog["id_kupca"] = izbor
+                novi_nalog["id_filma"] = izbor_film
+                for item1 in popis_korisnika:
+                    if item1["id"] == izbor:
+                        novi_nalog["ime_kupca"] = item1["ime"]
+                for item2 in popis_filmova:
+                    if item2["id"] == izbor_film:
+                        novi_nalog["ime_filma"] = item2["ime"]
+                novi_nalog["vrijeme_kreiranja"] = dt.now().strftime("%Y-%m-%d")
+                lista.append(novi_nalog)
+                print("Podaci uspješno uneseni. Povratak u glavni izbornik...")
+                time.sleep(2)
+                ocisti_ekran()
+
+def search(ime):
+    lista_pretrage = ime.split()
+    novi_popis = []
+    for item in popis_filmova:
+        for word in lista_pretrage:
+            if word in item["ime"].lower():
+                novi_popis.append(item)
+    novi_popis = [i for n, i in enumerate(novi_popis) if i not in novi_popis[:n]]
+    if novi_popis == []:
+        return 0
+    else:
+        t = PrettyTable(["ID", "IME FILMA", "GODINA FILMA"], padding_width = 3, hrules=ALL)
+        for item in novi_popis:
+            t.add_row([str(item["id"]), item["ime"], dt.fromisoformat(item["datum"]).strftime("%Y")])
+        print(f"{t}\n")
+
+if __name__ == "__main__":
+    flag = True
+    ocisti_ekran()
+    while flag:
+        opcija = unosOpcije(glavniIzbornik)
+        if opcija == 0:
+            flag = False
+        elif opcija == 1:
+            funkcijaIspisa(popis_korisnika)
+        elif opcija == 2:
+            funkcijaIspisa(izdani_filmovi, 1)
+        elif opcija == 3:
+            posudiFilm(izdani_filmovi)
+        elif opcija == 4:
+            pass
