ff17f151319963670ed40b5752e6f4867a486134
Dino Cerjak <d.cerjak@yahoo.com>
2022-11-10 23:14:43 +0100

    new file:   Algebra/Vjezbe/quiz/score.txt

--- Algebra/Vjezbe/quiz/main.py
+++ Algebra/Vjezbe/quiz/main.py
@@ -1,9 +1,14 @@
 import os
+import random
+from string import ascii_lowercase
+import time
 
-fname = "/config/workspace/Algebra_workspace/Algebra/Vjezbe/quiz/pitanja.txt" 
+fname_r = "/config/workspace/Algebra_workspace/Algebra/Vjezbe/quiz/pitanja.txt" 
+fname_a = "/config/workspace/Algebra_workspace/Algebra/Vjezbe/quiz/score.txt"
+QUESTIONS = {}
 
 def readFile(ime: str):
-    exists = os.path.isfile(ime)    
+    exists = os.path.isfile(ime)   #True/False 
     if exists:
         try:
             file = open(ime, "r")
@@ -15,8 +20,50 @@ def readFile(ime: str):
     else:
         print("File ne postoji!")
 
-linije = readFile(fname)
+def appendToFile(s : str, fname: str):
+    file = open(fname, "a")
+    file.write(s)
+    file.close()
 
-for linija in linije:
-    elementi = linija.strip().split(";")
-    print(elementi)
+def prepareQuestions(fname_r):
+
+    linije = readFile(fname_r)
+    for linija in linije:
+        elementi = linija.strip().split(";")
+        elementi.pop(0)
+        QUESTIONS[elementi[0]] = [elementi[1], elementi[2], elementi[3], elementi[4], elementi[5]]
+
+def start(QUESTIONS, fname_r):
+
+    prepareQuestions(fname_r)
+
+    for num, (question, choices) in enumerate(QUESTIONS.items(), start=1):
+        ocisti_ekran()
+        print(f"\nPitanje {num}:")
+        print(f"{question}?")
+        choices = choices.copy()
+        correct_answer = choices[-1]
+        choices.pop(-1)
+        labeled_choices = dict(zip(ascii_lowercase, sorted(choices)))
+        for label, choice in labeled_choices.items():
+            print(f"  {label}) {choice}")
+        
+        while (answer_label := input("\nOdgovor: ")) not in labeled_choices:
+            print(f"Molim unesi jedan od {', '.join(labeled_choices)} odgovora.")
+
+        answer = labeled_choices[answer_label]
+        if answer == correct_answer:
+            print("⭐ Točno! ⭐")
+            time.sleep(2)
+        else:
+            print(f'''Odgovor je '{correct_answer}', a ne '{answer}' ''')
+            time.sleep(2)
+
+def ocisti_ekran():
+    os.system("cls" if os.name == "nt" else "clear")
+
+if __name__ == "__main__":
+
+    start(QUESTIONS, fname_r)
+    
+    
